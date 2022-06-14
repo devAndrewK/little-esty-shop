2c3a45691a779df69b6a0b66becc6255588f0e87
Andrew King <dev.andrewking@gmail.com>
2022-06-14 00:42:44 -0500

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_165931) do
+ActiveRecord::Schema.define(version: 2022_06_14_044016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_06_10_165931) do
     t.datetime "updated_at", null: false
     t.bigint "item_id"
     t.bigint "invoice_id"
+    t.bigint "discount_id"
+    t.index ["discount_id"], name: "index_invoice_items_on_discount_id"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
     t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
@@ -78,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_06_10_165931) do
   end
 
   add_foreign_key "discounts", "merchants"
+  add_foreign_key "invoice_items", "discounts"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
